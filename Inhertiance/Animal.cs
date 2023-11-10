c0d019c6200bdae3c36e118ad48598f4130556b1
Administrator <Administrator@DESKTOP-95RB6IG>
2023-11-10 17:29:49 +0100

    Aggiungere i file di progetto.

---
+++ Inhertiance/Animal.cs
@@ -0,0 +1,21 @@
+﻿namespace Inhertiance
+{
+    public class Animal
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public Animal(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        public virtual void Speak()
+        {
+            Console.WriteLine($"Verse of the {Name}");
+        }
+    }
+}
---
+++ Inhertiance/Dog.cs
@@ -0,0 +1,15 @@
+﻿namespace Inhertiance
+{
+    public class Dog : Animal
+    {
+        public Dog(string Name, int Age) : base(Name, Age)
+        {
+        }
+
+
+        public override void Speak()
+        {
+            Console.WriteLine($"{Name} of {Age} years ago woof woof");
+        }
+    }
+}
